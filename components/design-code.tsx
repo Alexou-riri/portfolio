cab0663e704affd01ab74578d5fb54017242cfc0
Alexandra Rieux <alexandra.rieux@gmx.com>
2022-01-10 16:37:06 +0100

    Modif info

--- components/design-code.tsx
+++ components/design-code.tsx
@@ -1,27 +1,28 @@
-import React from "react";
-import Image from 'next/image'
-
-import { Container } from "./container";
-import { StyledDesignPlus } from "./styles/index.styles";
+import Image from 'next/image';
+import React from 'react';
+import { Container } from './container';
+import { StyledDesignPlus } from './styles/index.styles';
 
 const DesignCode = () => {
   return (
     <StyledDesignPlus>
       <Container className="container">
         <div className="text">
-          <h2>Design+Code</h2>
+          <h2>Logistic+Code</h2>
           <p>
-            In the last 3 years, I've been an engineer, a designer and an
-            engineer again.
-          </p>
-          <p>
-            Most of my everyday work falls in between. Having this perspective
-            puts me in a good position to suggest design solutions and quickly
-            prototype them with code. And I'm loving every part of it!
+            In the last 5 years, I've been working in logistics and seeing
+            different problems and situation that could be fixed or changed with
+            the right application.
           </p>
         </div>
         <div className="image">
-          <Image src="/images/article-preview.png" height={200} width={350} sizes="(min-width: 759px) 800px" alt="article preview" />
+          <Image
+            src="/images/article-preview.png"
+            height={200}
+            width={350}
+            sizes="(min-width: 759px) 800px"
+            alt="article preview"
+          />
         </div>
       </Container>
     </StyledDesignPlus>
